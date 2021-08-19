ea2796da87e36ee75f90ddccc2e2ab2b525caa39
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-08-19 20:47:31 +0000

    setup index and category views to show a table of all categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,16 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
   end
-  
+
 end
