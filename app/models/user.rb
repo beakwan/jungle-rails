3e3aee9ac772f9203535735a8372120ee24a561b
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-08-24 17:42:50 +0000

    add validation check for user signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   
 end
